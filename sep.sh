76880dff44722720a70cf4a612d505ab3d738ffb
PHASORDEVシ <103030954+PhasorDev@users.noreply.github.com>
2022-09-28 14:22:07 +0700

    Delete sep.sh

--- sep.sh
+++
@@ -1,7 +0,0 @@
-echo "# SodiumClient-Plugin" >> README.md
-git init
-git add .
-git commit -m "first commit"
-git branch -M main
-git remote add origin git@github.com:PhasorDev/SodiumClient-Plugin.git
-git push -u origin main
